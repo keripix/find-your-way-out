da1ca7e38b5ae2cf3c01c939156bba7158ecec6f
A. Akbar Hidayat <keripix@gmail.com>
2013-07-07 22:32:46 +0700

    simulating canvas drawing in nodejs

--- tests/worldGeneratorSpec.js
+++ tests/worldGeneratorSpec.js
@@ -1,4 +1,5 @@
-var wg = require("../lib/worldGenerator");
+var wg = require("../lib/worldGenerator"),
+    Canvas = require("canvas");
 
 describe("Generating World", function(){
   describe("Parsing Position", function(){
@@ -25,6 +26,32 @@ describe("Generating World", function(){
 
       expect(pos2.x).toEqual(6);
       expect(pos2.width).toEqual(5);
+    });
+  });
+
+  describe("Placing items on canvas", function(){
+    var canvas;
+
+    beforeEach(function(){
+      canvas = new Canvas(600, 600);
+    });
+
+    it("Should place item correctly with the correct dimension", function(){
+      var positions = [{x: 50,y: 50, color:"rgb(212,213,214)"}, {x: 121, y: 31, width: 10, height:20, color: "rgb(100,101,102)"}],
+          drawn = wg.generate(canvas, positions);
+
+      expect(drawn).toBeDefined();
+
+      // check wether the boxes has been drawn
+      var context = drawn.getContext('2d'),
+          data1 = context.getImageData(50, 50, 5, 5).data, // the first box
+          data2 = context.getImageData(121, 31, 10, 21).data; // the second box
+
+      for (var i = 0, n = data1.length; i < n; i += 4){
+        expect(data1[i]).toEqual(212);
+        expect(data1[i+1]).toEqual(213);
+        expect(data1[i+2]).toEqual(214);
+      }
     });
   });
 });

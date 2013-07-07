226657b8f523aacfdeb158e3a72109ba3b6bc822
A. Akbar Hidayat <keripix@gmail.com>
2013-07-07 11:51:39 +0700

    has passed the world

--- tests/levelSpec.js
+++ tests/levelSpec.js
@@ -2,3 +2,12 @@ var should = require("should"),
     level = require("../lib/level"),
     conf = require("../conf/game");
 
+describe("Game Configuration", function(){
+  describe("Reading Configuration", function(){
+    it("Should parse the configuration correctly", function(){
+      var gameConf = level.init(conf);
+
+      gameConf.should.have.ownProperty("world");
+    });
+  });
+});

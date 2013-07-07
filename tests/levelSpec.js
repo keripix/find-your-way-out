bbae91b466cd405a3e15feac40df69d2bf22cef5
A. Akbar Hidayat <keripix@gmail.com>
2013-07-07 12:15:45 +0700

    should have a module to parse level configuration

--- tests/levelSpec.js
+++ tests/levelSpec.js
@@ -1,11 +1,11 @@
 var should = require("should"),
-    level = require("../lib/level"),
+    gameConfig = require("../lib/gameConfiguration"),
     conf = require("../conf/game");
 
 describe("Game Configuration", function(){
   describe("Reading Configuration", function(){
     it("Should parse the configuration correctly", function(){
-      var gameConf = level.init(conf);
+      var gameConf = gameConfig.init(conf);
 
       gameConf.should.have.ownProperty("world");
       gameConf.should.have.ownProperty("levels");
@@ -20,8 +20,18 @@ describe("Game Configuration", function(){
 
     it("Should throw Error if bad conf is passed", function(){
       (function(){
-        level.init({bla:1,bli:2});
+        gameConfig.init({bla:1,bli:2});
       }).should.throw();
+    });
+
+    it("Should parsed the level correctly", function(){
+      var gameConf = gameConfig.init(conf);
+
+      gameConf.getLevel("1").should.be.ok;
+
+      var level1 = gameConf.getLevel("1");
+
+      level1.should.have.property("blocks");
     });
   });
 });

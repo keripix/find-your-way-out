f6a5735bd5ea51abd78246686a8b6e95cec21267
A. Akbar Hidayat <keripix@gmail.com>
2013-07-07 18:54:18 +0700

    parsing positions

--- tests/worldGeneratorSpec.js
+++ tests/worldGeneratorSpec.js
@@ -1,5 +1,13 @@
 var wg = require("../lib/worldGenerator");
 
 describe("Generating World", function(){
+  describe("Parsing Position", function(){
+    it("Should parse position correctly", function(){
 
+      var positions = wg.parsePosition({x: 59, y: 21});
+
+      expect(positions[0].x).toEqual(59);
+      expect(positions[0].y).toEqual(21);
+    });
+  });
 });

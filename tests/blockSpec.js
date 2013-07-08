bf30a14e74735cad4766b63c94401fc70b772704
A. Akbar Hidayat <keripix@gmail.com>
2013-07-08 21:14:10 +0700

    add movement method

--- tests/blockSpec.js
+++ tests/blockSpec.js
@@ -14,4 +14,16 @@ describe("Block Item", function(){
     expect(item.x).toEqual(10);
     expect(item.midX).toEqual(15);
   });
+
+  it("Should move and update property correctly", function(){
+    var item = block.create(conf);
+
+    item.moveX(10);
+    expect(item.x).toEqual(20);
+    expect(item.midX).toEqual(25);
+
+    item.moveY(10);
+    expect(item.x).toEqual(20);
+    expect(item.midX).toEqual(25);
+  });
 });

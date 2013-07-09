dbc66a18bb57c7039b83bb6620a7f507dcd7e52b
A. Akbar Hidayat <keripix@gmail.com>
2013-07-10 05:38:38 +0700

    tested

--- tests/awareSpec.js
+++ tests/awareSpec.js
@@ -1,7 +1,7 @@
-var aware = require("./../lib/aware"),
+var Aware = require("./../lib/aware"),
     block = require("./../lib/block"),
     actor = require("./../lib/actor"),
-    player, blocks, exit;
+    aware, player, blocks, exit, world;
 
 describe("I'm aware", function(){
   beforeEach(function(){
@@ -26,6 +26,10 @@ describe("I'm aware", function(){
       width: 10,
       height: 10
     });
+
+    world = {width: 150, height: 150};
+
+    aware = new Aware(player, blocks, exit, world);
   });
 
   afterEach(function(){
@@ -35,7 +39,7 @@ describe("I'm aware", function(){
   it("Should stop when the distance is to close", function(){
     while(true){
       player.moveX(5);
-      aware(player, blocks, exit,{width:150,height:150});
+      aware.observe();
       if (!player.isMoving){
         break;
       }
@@ -52,7 +56,7 @@ describe("I'm aware", function(){
 
     while(player.x <= exit.x){
       player.moveX(5);
-      aware(player, blocks, exit,{width:150,height:150});
+      aware.observe();
 
       if (!player.isMoving){
         break;
@@ -65,10 +69,14 @@ describe("I'm aware", function(){
   it("Should lose if the player has gone out of the world", function(){
     blocks[0].y = 100;
     blocks[0].midY = 105;
+    exit.x =  145;
+    exit.y = 50;
+    exit.midX = 150;
+    exit.midY = 55;
 
     while(true){
       player.moveX(5);
-      aware(player, blocks, {x: 145,y:50,midX:150,midY:55},{width:150,height:150});
+      aware.observe();
       if (!player.isMoving){
         break;
       }
@@ -80,7 +88,7 @@ describe("I'm aware", function(){
   it("Should normalize player position after collision with block", function(){
     while (true) {
       player.moveX(7);
-      aware(player, blocks, exit, {width: 150, height: 150});
+      aware.observe();
 
       if (!player.isMoving){
         break;
@@ -93,7 +101,7 @@ describe("I'm aware", function(){
   it("Should be able to detect where the stopper's location is", function(){
     for(var i=player.x;i<=50;i+=5){
       player.moveX(5);
-      aware(player, blocks, exit,{width:150,height:150});
+      aware.observe();
     }
 
     expect(player.isMoving).toBeFalsy();

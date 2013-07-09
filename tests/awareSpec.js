22a830248c780453b962a5b820e02d97e5afb510
A. Akbar Hidayat <keripix@gmail.com>
2013-07-09 09:58:18 +0700

    tested

--- tests/awareSpec.js
+++ tests/awareSpec.js
@@ -1,24 +1,7 @@
 var aware = require("./../lib/aware"),
     block = require("./../lib/block"),
     actor = require("./../lib/actor"),
-    player = actor.create({
-      x: 10,
-      y: 10,
-      width: 10,
-      height: 10
-    }),
-    blocks = [block.create({
-      x: 50,
-      y: 12,
-      width: 10,
-      height: 10
-    })],
-    exit = block.create({
-      x: 100,
-      y: 100,
-      width: 10,
-      height: 10
-    });
+    player, blocks, exit;
 
 describe("I'm aware", function(){
   beforeEach(function(){
@@ -29,17 +12,35 @@ describe("I'm aware", function(){
       height: 10
     });
     player.isMoving = true;
+
+    blocks = [block.create({
+      x: 50,
+      y: 12,
+      width: 10,
+      height: 10
+    })];
+
+    exit = block.create({
+      x: 100,
+      y: 100,
+      width: 10,
+      height: 10
+    });
+  });
+
+  afterEach(function(){
+    player = blocks = exit = null;
   });
 
   it("Should stop when the distance is to close", function(){
-    var iterate = 0;
-    while(player.isMoving){
-      if (++iterate >= 10){
-        break;
-      }
+    while(true){
       player.moveX(5);
       aware(player, blocks, exit,{width:150,height:150});
+      if (!player.isMoving){
+        break;
+      }
     }
+
     expect(player.isMoving).toBeFalsy();
   });
 
@@ -52,28 +53,41 @@ describe("I'm aware", function(){
     while(player.x <= exit.x){
       player.moveX(5);
       aware(player, blocks, exit,{width:150,height:150});
+
+      if (!player.isMoving){
+        break;
+      }
     }
 
     expect(player.hasWon).toBeTruthy();
   });
 
   it("Should lose if the player has gone out of the world", function(){
-    while(player.x <= 150){
+    blocks[0].y = 100;
+    blocks[0].midY = 105;
+
+    while(true){
       player.moveX(5);
       aware(player, blocks, {x: 145,y:50,midX:150,midY:55},{width:150,height:150});
+      if (!player.isMoving){
+        break;
+      }
     }
 
     expect(player.hasLost).toBeTruthy();
   });
 
   it("Should normalize player position after collision with block", function(){
-    while (player.x <= blocks[0].x) {
+    while (true) {
       player.moveX(7);
       aware(player, blocks, exit, {width: 150, height: 150});
+
+      if (!player.isMoving){
+        break;
+      }
     }
 
-    expect(player.isMoving).toBeFalsy();
-    expect(player.x).toEqual(50);
+    expect(player.x).toEqual(40);
   });
 
   it("Should be able to detect where the stopper's location is", function(){

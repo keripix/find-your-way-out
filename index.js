044da18b918f6d541d78672e16454c2f5deae44a
A. Akbar Hidayat <keripix@gmail.com>
2013-07-15 20:28:11 +0700

    change name

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var shell = require("game-shell")(),
     Aware = require("./lib/aware"),
     GamePlay = require("./lib/gamePlay");
 
-var VERSION = "0.3.8";
+var VERSION = "0.3.9";
 
 // Initiate shell
 shell.on("init", function(){
